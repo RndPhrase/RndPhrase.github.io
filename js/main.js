ebde920b97af3a68372073a48a1c74ddc6da9854
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2014-09-29 16:48:57 +0200

    Added configuration UI components

--- js/main.js
+++ js/main.js
@@ -4,6 +4,25 @@ require(['rndphrase'], function(RndPhrase) {
 		self.seed = ko.observable('');
 		self.password = ko.observable('');
 		self.uri = ko.observable('');
+		self.size = ko.observable(16);
+		self.version = ko.observable(1);
+
+		self.numeric_min = ko.observable(1);
+		self.numeric_max = ko.observable(-1);
+		self.numeric_alphabet = ko.observable('0123456789');
+
+		self.capital_min = ko.observable(1);
+		self.capital_max = ko.observable(-1);
+		self.capital_alphabet = ko.observable('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+		self.minuscule_min = ko.observable(1);
+		self.minuscule_max = ko.observable(-1);
+		self.minuscule_alphabet = ko.observable('abcdefghijklmnopqrstuvwxyz');
+
+		self.special_min = ko.observable(1);
+		self.special_max = ko.observable(-1);
+		self.special_alphabet = ko.observable(' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~');
+
 		self.hash = ko.pureComputed(function() {
 			if(self.seed() && self.password() && self.uri()) {
 				var r = new RndPhrase({
@@ -16,4 +35,8 @@ require(['rndphrase'], function(RndPhrase) {
 		}, this);
 	}
 	ko.applyBindings(new viewModel());
+
+	$('#hash').on('focus', function(e) {
+		$(this).select()
+	});
 });
